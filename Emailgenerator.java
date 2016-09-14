15fbca5682c1177c59a7292dfa0678cd8fcb076e
Samuel Kuznia <skuznia@cbeagan.org>
2016-09-14 15:28:48 -0500

    Adding random email generator test

---
+++ Emailgenerator.java
@@ -0,0 +1,17 @@
+package repocheck;
+import java.util.Random;
+
+public class Emailgenerator {
+	public static void main(String[] args){
+		System.out.println(emailgen());
+	}
+	public static String emailgen(){
+		Random randgen = new Random();
+		String numconv = Integer.toString(randgen.nextInt(999999));
+		String email = numconv + "@mailinator.com";
+		return email;
+		
+	}
+	
+
+}
